149c8d9e1c3d9128299dd1c9363d753e0cc27533
Oksana Lorents <oksana.lorents@gmail.com>
2024-08-17 12:39:03 -0700

    API and Selenium Tests added

---
+++ Pages/SubmitPage.cs
@@ -0,0 +1,88 @@
+﻿using OpenQA.Selenium;
+
+
+namespace AdvertisingSeleniumtests.Pages
+{
+    internal class SubmitPage
+    {
+        private readonly IWebDriver _driver;
+        public SubmitPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        private IWebElement firstNameWebElement => _driver.FindElement(By.CssSelector("input[placeholder='Enter First Name...']"));
+        private IWebElement lastNameWebElement => _driver.FindElement(By.CssSelector("input[placeholder='Enter Last Name...']"));
+        private IWebElement emailWebElement => _driver.FindElement(By.CssSelector("input[placeholder='Enter Email...']"));
+        private IWebElement phoneWebElement => _driver.FindElement(By.CssSelector("input[placeholder='Enter Phone...']"));
+        private IWebElement stateWebElement => _driver.FindElement(By.CssSelector("input[placeholder='Enter State...']"));
+        private IWebElement reasonWebElement => _driver.FindElement(By.CssSelector("input[placeholder='Enter Reason...']"));
+        private IWebElement submitButton => _driver.FindElement(By.XPath("//button[@type='submit']"));
+        private IWebElement firstNameRequired => _driver.FindElement(By.XPath("//span[contains(@class, 'add-set-error') and text()='First Name Required']"));
+
+        public void EnterFirstName(string text)
+        {
+            firstNameWebElement.SendKeys(text);
+        }
+
+        public void EnterLastName(string text)
+        {
+            lastNameWebElement.SendKeys(text);
+        }
+
+        public void EnterEmail(string text)
+        {
+            emailWebElement.SendKeys(text);
+        }
+
+        public void EnterPhone(string text)
+        {
+            phoneWebElement.SendKeys(text);
+        }
+
+        public void EnterState(string text)
+        {
+            stateWebElement.SendKeys(text);
+        }
+
+        public void EnterReason(string text)
+        {
+            reasonWebElement.SendKeys(text);
+        }
+
+        public string GetFistNameFieldValue()
+        {
+            return firstNameWebElement.GetAttribute("value");
+        }
+
+        public string GetLastNameFieldValue()
+        {
+            return lastNameWebElement.GetAttribute("value");
+        }
+        public string GetEmailFieldValue()
+        {
+            return emailWebElement.GetAttribute("value");
+        }
+        public string GetPhoneFieldValue()
+        {
+            return phoneWebElement.GetAttribute("value");
+        }
+        public string GetStateFieldValue()
+        {
+            return stateWebElement.GetAttribute("value");
+        }
+
+        public string GetReasonFieldValue()
+        {
+            return reasonWebElement.GetAttribute("value");
+        }
+        public void ClickSubmitButton()
+        {
+            submitButton.Click();
+        }
+        public string FirstNameRequired()
+        {
+            return firstNameRequired.Text;
+        }
+    }
+}
---
+++ Tests/SubmitInfoTests.cs
@@ -0,0 +1,100 @@
+﻿using AdvertisingSeleniumtests.Pages;
+using AutomationFramework.Config;
+using AutomationFramework.Driver;
+using OpenQA.Selenium;
+
+namespace AdvertisingSeleniumtests.Tests
+{
+    internal class SubmitInfoTests
+    {
+        IWebDriver _driver;
+        SubmitPage _submitPage;
+
+       [SetUp]
+        public void SetUp()
+        {
+            var testSettings = new TestSettings
+            {
+                BrowserType = DriverFixtures.BrowserType.Chrome,
+                ApplicationUrl = new Uri("https://automationtestwaui.azurewebsites.net/")
+            };
+
+            _driver = new DriverFixtures(testSettings).Driver;
+            _submitPage = new SubmitPage(_driver);
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+
+
+        [Test]
+        public void ContactInfo_ShouldBeSetSuccessfully()
+        {
+
+            //arrange
+            var expectedFirstName = "Test First Name";
+            var expectedLastName = "Test Last Name";
+            var expectedEmail = "Test Email";
+            var expectedPhone = "Test Phone";
+            var expectedState = "Test State";
+            var expectedReason = "Test Reason";
+
+            //act
+            _submitPage.EnterFirstName(expectedFirstName);
+            _submitPage.EnterLastName(expectedLastName);
+            _submitPage.EnterEmail(expectedEmail);
+            _submitPage.EnterPhone(expectedPhone);
+            _submitPage.EnterState(expectedState);
+            _submitPage.EnterReason(expectedReason);
+
+
+            //assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(_submitPage.GetFistNameFieldValue(), Is.EqualTo(expectedFirstName), "First name value is not correct");
+                Assert.That(_submitPage.GetLastNameFieldValue(), Is.EqualTo(expectedLastName), "Last name value is not correct");
+                Assert.That(_submitPage.GetEmailFieldValue(), Is.EqualTo(expectedEmail), "Email value is not correct");
+                Assert.That(_submitPage.GetPhoneFieldValue(), Is.EqualTo(expectedPhone), "Phone value is not correct");
+                Assert.That(_submitPage.GetStateFieldValue(), Is.EqualTo(expectedState), "State value is not correct");
+                Assert.That(_submitPage.GetReasonFieldValue(), Is.EqualTo(expectedReason), "Reason value is not correct");
+            });
+        }
+
+        [Test]
+        public void FirstNameFielld_ShouldNotBeEmpty()
+        {
+            //arrange
+            var expectedLastName = "Test Last Name";
+            var expectedEmail = "Test Email";
+            var expectedPhone = "Test Phone";
+            var expectedState = "Test State";
+            var expectedReason = "Test Reason";
+
+            //act
+            _submitPage.EnterLastName(expectedLastName);
+            _submitPage.EnterEmail(expectedEmail);
+            _submitPage.EnterPhone(expectedPhone);
+            _submitPage.EnterState(expectedState);
+            _submitPage.EnterReason(expectedReason);
+            _submitPage.ClickSubmitButton();
+
+
+            //assert
+            Assert.Multiple(() =>
+            {
+
+                Assert.That(_submitPage.FirstNameRequired(), Is.EqualTo("First Name Required"), "First Name Required label is missing");
+                Assert.That(_submitPage.GetLastNameFieldValue(), Is.EqualTo(expectedLastName), "Last name value is not correct");
+                Assert.That(_submitPage.GetEmailFieldValue(), Is.EqualTo(expectedEmail), "Email value is not correct");
+                Assert.That(_submitPage.GetPhoneFieldValue(), Is.EqualTo(expectedPhone), "Phone value is not correct");
+                Assert.That(_submitPage.GetStateFieldValue(), Is.EqualTo(expectedState), "State value is not correct");
+                Assert.That(_submitPage.GetReasonFieldValue(), Is.EqualTo(expectedReason), "Reason value is not correct");
+            });
+        }
+
+    }
+}
